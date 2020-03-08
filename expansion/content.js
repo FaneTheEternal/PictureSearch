adc9d1e51f6506ce4deb3cd487ff33e7c036c19a
FaneTheEternal <artemyev.vova@gmail.com>
2020-03-08 21:52:49 +0300

    hotfix

--- expansion/content.js
+++ expansion/content.js
@@ -25,16 +25,14 @@ document.addEventListener('picture_search', () => {
                 const src = el.src;
                 let size = {w: 0, h: 0};
 
-                if (el.style.width) size.w = el.style.width.toString();
-                else size.w = el.parentNode.style.width.toString();
-                size.w = size.w ? size.w.slice(0, length - 3) : 0;
+                if (el.style.width) size.w = el.style.width.toString().slice(0, length - 2);
+                else size.w = el.getBoundingClientRect().width;
 
-                if (el.style.height) size.h = el.style.height.toString();
-                else size.k = el.parentNode.style.height.toString();
-                size.h = size.h ? size.h.slice(0, length - 3) : 0;
+                if (el.style.height) size.h = el.style.height.toString().slice(0, length - 2);
+                else size.k = el.getBoundingClientRect().height;
 
-                // size.w > picSize && size.h > picSize
-                if (size.w > picSize && size.h > picSize) {
+                // size.w > picSize || size.h > picSize
+                if (size.w > picSize || size.h > picSize) {
                     theArray[index].title = `width: ${size.w} - height: ${size.h}`;
                     count++;
                 }
@@ -57,8 +55,8 @@ document.addEventListener('picture_search', () => {
                     if (el.style.height) size.h = el.style.height.toString().slice(0, length - 3);
                     else size.h = el.getBoundingClientRect().height;
 
-                    // size.w > picSize && size.h > picSize
-                    if (size.w > picSize && size.h > picSize) {
+                    // size.w > picSize || size.h > picSize
+                    if (size.w > picSize || size.h > picSize) {
                         theArray[index].title = `width: ${size.w} - height: ${size.h}`;
                         count++;
                     }
